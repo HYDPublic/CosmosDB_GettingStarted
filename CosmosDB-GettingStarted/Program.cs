63e4ad804c66b54e170bec1d1843284cf4d68260
James Earle <jaearle@microsoft.com>
2017-09-12 12:02:59 -0700

    initial project creation

--- CosmosDB-GettingStarted/Program.cs
+++ CosmosDB-GettingStarted/Program.cs
@@ -1,9 +1,20 @@
 ﻿using System;
 
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using Newtonsoft.Json;
+
 namespace CosmosDB_GettingStarted
 {
     class Program
     {
+        private const string EndpointUri = "https://core-cosmo-cs-sql.documents.azure.com:443/";
+        private const string PrimaryKey = "MktS98Vx9ge5G1s6nf7Sc3Ipkuifb88aKG18CNUvhmmy4AOWDhm8ocWmCkt6DXI8NnX3zvmG39ojzi6AfT6CVw==";
+        private DocumentClient client;
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
